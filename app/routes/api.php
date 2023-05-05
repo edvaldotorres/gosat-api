bb4d55db209166976cb0325080e7a65217fd98d8
edvaldotorres <edvaldo.torres@tisaude.com>
2023-05-05 16:54:27 -0300

    :recycle: (refactor): Refactor code

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\CreditController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,3 +13,5 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/v1/credits/offers', [CreditController::class, 'creditOffer']);
